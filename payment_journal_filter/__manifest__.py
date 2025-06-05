636d2f8ef90fdbe0ddc98df56269750040ab8ae6
pjabie <pjabie@broobe.com>
2025-06-05 12:38:52 -0500

    se agrega filtro para las transferencias internas

---
+++ payment_journal_filter/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Filtro de diarios archivados en pagos",
+    "version": "16.0.1.0.1",
+    "depends": ["account"],
+    "author": "Gauchocode",
+    "description": "Evita que se seleccionen diarios archivados en pagos",
+    "category": "Accounting",
+    "license": "LGPL-3",
+    "data": [
+        "views/account_payment_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
